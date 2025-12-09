d05d813a33e423bb891bf1a0ce53ebb09a411e34
Clara Loforti <clara.loforti@gmail.com>
2025-12-09 15:57:12 +0100

    es.iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,12 +1,21 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi e restituisca un nuovo array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function trovaIniziali(myArray) {
+    const iniziali = [];
+    for (let i = 0; i < myArray.length; i++) {
+        iniziali.push(myArray[i][0]); // aggiungo la prima lettera di ogni nome dell'array
+    }
+    return iniziali;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+const risultato = trovaIniziali(names);
+console.log(risultato);
 
 
 
