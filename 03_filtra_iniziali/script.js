44d83338e0bfcd16a1df0a0a404b973631a843fb
Clara Loforti <clara.loforti@gmail.com>
2025-12-09 16:08:27 +0100

    es.filtra iniziali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,9 +4,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function filterNames(myArray, initial) {
+    const filteredNames = [];
+    for (let i = 0; i < myArray.length; i++) {
+        if (myArray[i][0] === initial) { // se il nome inizia con initial allora aggiungo il nome all'array
+            filteredNames.push(myArray[i]);
+        }
+    }
+    return filteredNames;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+const initialA = filterNames (names, "A");
+console.log(initialA);
 
 
 
